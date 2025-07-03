60f99cc5dc6b22f60fbc9852638a6c75217d4494
estherdomfeh213 <domfehesther2020@gmail.com>
2025-07-03 19:51:04 +0000

    complete code for number guess game

--- number_guesser.py
+++ number_guesser.py
@@ -10,11 +10,33 @@ if top_of_range.isdigit():
         print("Please type a number larger than 0 next time.")
         quit()
     
-    else:
-        print("Please type a number next time.")
-        quit()
+else:
+    print("Please type a number next time.")
+    quit()
 
 random_number = random.randint(0, top_of_range)
-print(random_number)
+guesses = 0
+while True: 
+    guesses +=1
+    user_guess = input("Make a guess: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print("Please type a number next time.")
+        continue # automatic brings the program to the start of the program. 
+
+    if user_guess == random_number:
+        print("You got it!")
+        break
+
+    elif user_guess > random_number:
+            print("You were above the number!")
+
+    else:
+         print("You were below the number!")
+        
+
+
+print("You got it in", guesses, "guesses")
 
 
